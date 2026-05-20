ec1f058d5b64d4093b80041bb27e3f2163c5092d
BenjaminBaigorria <benjamin.baigorria@outlook.com>
2026-05-19 23:55:23 -0300

    feat(API/magic): adding a route to serch by name

--- API/magic.js
+++ API/magic.js
@@ -32,4 +32,19 @@ route.get("/magic/card/:id",(req,res)=>{
     
 })
 
+route.get("/magic/card/:name",(req,res)=>{
+    try {
+        const {name}= req.params;
+        fetch(`https://api.magicthegathering.io/v1/cards?name=${name}`)
+        .then(response => response.json())
+        .then(data =>{
+            res.set("Content-Type","application/json");
+            res.status(200).send(data);
+        })
+    } catch (error) {
+        res.status(400).send("Something is wrong");
+    }
+})
+
+
 module.exports=route;
