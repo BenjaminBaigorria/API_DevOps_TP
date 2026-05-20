19fd8a5de05e24004b1553cd308932b4d4cb5cda
BenjaminBaigorria <benjamin.baigorria@outlook.com>
2026-05-19 21:10:07 -0300

    update(PORT): adding port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv")
 
 dotenv.config();
 
-const PORT=process.env.PORT;
+const PORT=process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log(`Server listen on ${PORT} PORT`);
 })
