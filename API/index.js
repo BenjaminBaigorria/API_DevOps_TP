973a718043c50e6cac2b9caa74d602a55df15c8c
BenjaminBaigorria <benjamin.baigorria@outlook.com>
2026-05-21 11:58:30 -0300

    feat(/error): adding /error endpoint

--- API/index.js
+++ API/index.js
@@ -10,7 +10,7 @@ app.use(magic);
 app.get("/",(req,res)=>{
     try {
         res.set("Content-Type","text/plain");
-        res.status(200).send("API funcionando, puedes probar los endpoints: \n/hello\n/magic/cards\n/magic/card/:id\n/magic/cardname/:name");
+        res.status(200).send("API funcionando, puedes probar los endpoints: \n/hello\n/magic/cards\n/magic/card/:id\n/magic/cardname/:name\n/error");
     } catch (error) {
         res.status(400).send("Something is wrong");
     }
@@ -26,6 +26,11 @@ app.get("/hello",(req,res)=>{
     }
     
 })
+app.get("/error", (req, res) => {
+    res.status(500).json({
+        error: "Fallo intencional"
+    });
+});
 
 
 
